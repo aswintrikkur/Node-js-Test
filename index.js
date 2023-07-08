52c4f53bc925c5cbbcaa5a2f9bfb2c2ce5417e08
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-07-08 22:12:07 +0530

    module export and import

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('Introduction to Node js');
+
+const {add,sub}= require('./util')
+const path =require('path')
+
+console.log('sum=',add(10,20));
+console.log(`difference=`, sub(10,20));
+
+console.log(__filename);
+console.log(__dirname.toUpperCase());
+
+console.log(path.parse(__dirname));
